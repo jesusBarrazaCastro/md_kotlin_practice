61c2c3a92c984d6ff353c55ce2dd6a69b5d17ebb
Jesus Barraza <l21170157@culiacan.tecnm.mx>
2024-10-17 09:56:10 -0700

    <update> images

--- app/src/main/java/com/example/myapplication/oct15.kt
+++ app/src/main/java/com/example/myapplication/oct15.kt
@@ -1,72 +1,44 @@
 package com.example.myapplication
-
-import android.icu.text.DecimalFormat
-import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AddCircle
-import androidx.compose.material3.Button
-import androidx.compose.material3.DrawerState
-import androidx.compose.material3.DrawerValue
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ModalDrawerSheet
-import androidx.compose.material3.ModalNavigationDrawer
-import androidx.compose.material3.NavigationDrawerItem
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.rememberDrawerState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.ColorPainter
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.input.KeyboardType
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.myapplication.ui.theme.MyApplicationTheme
+
 
 class oct15 {
 
     @Composable
-    fun Example0815(modifier: Modifier = Modifier) {
-        Column(
-            verticalArrangement = Arrangement.spacedBy(18.dp),
-            modifier = modifier
-                .padding(20.dp)
-        ) {
-
+    fun imagePractice(){
+        var image = painterResource(R.drawable.mando)
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ){
+            Image(
+                painter = image,
+                "The mandalorian",
+                contentScale = ContentScale.Inside,
+                alignment = Alignment.Center,
+                modifier = Modifier
+                    .fillMaxSize()
+            )
         }
     }
 
     @Preview(showBackground = true)
     @Composable
     fun GreetingPreview() {
-        MyApplicationTheme {
-            Example0815()
-        }
-
+        imagePractice()
     }
 }
